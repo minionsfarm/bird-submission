e8cc41ac9266c3700ab27654be08a2aebb5dbfb0
dokook <dokook@minions.farm>
2024-06-14 18:45:27 -0700

    add scripts

---
+++ prepare-eval.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if ! command -v az &> /dev/null
+then
+    echo "Error: az command not found. Please install Azure CLI."
+    exit 1
+fi
+
+cat <<EOF > /etc/docker/daemon.json
+{
+  "runtimes": {
+    "nvidia": {
+      "path": "nvidia-container-runtime",
+      "runtimeArgs": []
+    }
+  }
+}
+EOF
+
+# Download models and adapters
+ACCOUNT_NAME="minionsbirdsubmission2"
+CONTAINER_NAME="models"
+user=$(whoami)
+DESTINATION_FOLDER="/home/${user}"
+
+TARGET="models/Meta-Llama-3-70B-Instruct/*"
+az storage blob download-batch \
+  --account-name $ACCOUNT_NAME \
+  --overwrite \
+  -s $CONTAINER_NAME \
+  -d $DESTINATION_FOLDER \
+  --pattern $TARGET
+echo "Download completed: $DESTINATION_FOLDER/$TARGET"
+
+TARGET="models/Meta-Llama-3-8B-Instruct/*"
+az storage blob download-batch \
+  --account-name $ACCOUNT_NAME \
+  --overwrite \
+  -s $CONTAINER_NAME \
+  -d $DESTINATION_FOLDER \
+  --pattern $TARGET
+echo "Download completed: $DESTINATION_FOLDER/$TARGET"
+
+TARGET="adapters/20240603_052455/*"
+az storage blob download-batch \
+  --account-name $ACCOUNT_NAME \
+  --overwrite \
+  -s $CONTAINER_NAME \
+  -d $DESTINATION_FOLDER \
+  --pattern $TARGET
+echo "Download completed: $DESTINATION_FOLDER/$TARGET"
+
+TARGET="adapters/20240528_220303/*"
+az storage blob download-batch \
+  --account-name $ACCOUNT_NAME \
+  --overwrite \
+  -s $CONTAINER_NAME \
+  -d $DESTINATION_FOLDER \
+  --pattern $TARGET
+echo "Download completed: $DESTINATION_FOLDER/$TARGET"
+
+# Download a docker
+docker pull minions.azurecr.io/bird
---
+++ run-eval.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+HOME="/home/$(whoami)"
+
+# Check if the number of arguments is 4
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <SPLIT> <MODEL> <INPUT_DIR> <OUTPUT_DIR>"
+    exit 1
+fi
+
+SPLIT=$1
+# Check if SPLIT is either dev or test
+if [ "$SPLIT" != "dev" ] && [ "$SPLIT" != "test" ]; then
+    echo "Error: SPLIT must be either 'dev' or 'test'"
+    exit 1
+fi
+
+MODEL=$2
+# Check if MODEL is either dev or test
+if [ "$MODEL" != "small" ] && [ "$MODEL" != "large" ]; then
+    echo "Error: MODEL must be either 'small' or 'large'"
+    exit 1
+fi
+
+# Map small to x and large to y
+if [ "$MODEL" == "small" ]; then
+    MODEL="20240603_052455"
+elif [ "$MODEL" == "large" ]; then
+    MODEL="20240528_220303"
+fi
+
+INPUT_DIR=$3
+OUTPUT_DIR=$4
+
+# Check if INPUT_DIR starts with $HOME and is not equal to $HOME
+if [[ "$INPUT_DIR" != "$HOME"* || "$INPUT_DIR" == "$HOME" ]]; then
+    echo "Error: INPUT_DIR must start with $HOME and not be equal to $HOME"
+    exit 1
+fi
+
+# Check if OUTPUT_DIR starts with $HOME and is not equal to $HOME
+if [[ "$OUTPUT_DIR" != "$HOME"* || "$OUTPUT_DIR" == "$HOME" ]]; then
+    echo "Error: OUTPUT_DIR must start with $HOME and not be equal to $HOME"
+    exit 1
+fi
+
+# Replace $HOME in INPUT_DIR with /home/root
+INPUT_DIR="${INPUT_DIR/#$HOME/\/home\/root}"
+OUTPUT_DIR="${OUTPUT_DIR/#$HOME/\/home\/root}"
+
+echo docker run \
+    --rm \
+    --gpus all \
+    --runtime=nvidia \
+    -v "${HOME}:/home/root minions.azurecr.io/bird" \
+    --model_id="${MODEL}" \
+    --split="${SPLIT}" \
+    --dataset="bird_${SPLIT}" \
+    --input_dir="${INPUT_DIR}" \
+    --output_dir="${OUTPUT_DIR}" \
+    --num_gpus=4
